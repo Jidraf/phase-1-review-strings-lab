5551ee47ae244ef7956043fd81c0d612e2e5460e
Jidraf Kimachia <raphjid170@gmail.com>
2022-06-17 09:51:09 +0300

    phase-1-review-strings-lab

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const currentUser = 'Grace Hopper';
-const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!';
+// Write your code in this file!
+
+
+const currentUser = "Jidraph"
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = Welcome, ${currentUser.slice(0, 1)}! ;
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
